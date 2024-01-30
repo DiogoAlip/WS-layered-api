927445a14ef626eb4ce0239b23329cd45f9cda79
Diogo <diogoalipazaga@gmail.com>
2024-01-29 23:38:02 -0500

    feat: styles and db added

--- Negocio/negocio.js
+++ Negocio/negocio.js
@@ -1,23 +1,3 @@
-const controller = {}
-const mysql = require('mysql')
-const url =require('../routes/database')
-const dbconnection = mysql.createConnection(url.urlMysql)
+import { arr } from "/data.js";
 
-
-controller.list  = (req, res) => {
-  dbconnection.query("SELECT * FROM revist", (error, results, fields)=>{
-
-    res.render("Revista",{
-        data: results
-    })
-});
-}
-
-controller.agregar = (req, res) =>{
-    const data=req.body
-   dbconnection.query('INSERT INTO revist set ?',[data], (err,results)=>{
-       res.redirect('/')
-   })
-}
-
-module.exports = controller
+let socket = io();
--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on("connection", (socket) => {});
+io.on("connection", (socket) => {
+  console.log("im in");
+});
 
 //app.use(express.static(`${__dirname}/presentacion/index.html`));
+app.use(express.static(path.join(__dirname, "Negocio")));
+app.use(express.static(path.join(__dirname, "baseDeDatos")));
+app.use(express.static(path.join(__dirname, "presentacion")));
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/presentacion/index.html`);
